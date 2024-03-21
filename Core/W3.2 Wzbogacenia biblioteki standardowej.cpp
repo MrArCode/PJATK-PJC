c976b07d12378aed60fb6a23a9ffc17bb07dcb6f
MrArCode <a.c.posluszny@gmail.com>
2024-03-21 10:05:53 +0100

    W3.2

--- Core/W3.2 Wzbogacenia biblioteki standardowej.cpp
+++ Core/W3.2 Wzbogacenia biblioteki standardowej.cpp
@@ -1,3 +1,34 @@
-//
-// Created by Artur Cegiełka-Posłuszny on 21/03/2024.
-//
+#include <iostream>
+#include <vector>
+#include <fmt/ranges.h>
+#include <algorithm>
+#include <ranges>
+
+namespace pjc {
+    namespace ranges {
+        auto sort(std::vector<int> vector) -> std::vector<int> {
+            std::ranges::sort(vector);
+            return vector;
+        }
+
+        auto reverse(std::vector<int> vector) -> std::vector<int> {
+            std::ranges::reverse(vector);
+            return vector;
+        }
+    }
+}
+
+auto main() -> int {
+    auto vector = std::vector<int>{2, 4, 6, 1, 2, 5, 8, 9, 0, 12};
+    fmt::print("Original Vector: {}\n", vector);
+
+    auto sortedVector = pjc::ranges::sort(vector);
+    fmt::print("Sorted Vector: {}\n", sortedVector);
+
+    auto reversedVector = pjc::ranges::reverse(vector);
+    fmt::print("Reversed Vector: {}\n", reversedVector);
+
+    fmt::print("Original Vector: {}\n", vector);
+
+    return 0;
+}
