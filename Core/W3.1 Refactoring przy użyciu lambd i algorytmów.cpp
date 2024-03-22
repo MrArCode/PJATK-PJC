c9aa6c2031b7fbe0822f41dc952a0249c2c5936e
MrArCode <a.c.posluszny@gmail.com>
2024-03-22 16:04:54 +0100

    1

---
+++ Core/W3.1 Refactoring przy użyciu lambd i algorytmów.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <fmt/ranges.h>
+#include <algorithm>
+#include <ranges>
+#include <set>
+
+auto exampleMain(std::vector<std::string> &left, std::vector<std::string> &right) -> void {
+    auto eraseDuplicates = [](std::vector<std::string> &vec) -> void {
+        auto tmpSet = std::set<std::string>(vec.begin(), vec.end());
+        vec = std::vector<std::string>(tmpSet.begin(), tmpSet.end());
+    };
+    eraseDuplicates(left);
+    eraseDuplicates(right);
+    auto sortVector = [](std::vector<std::string> &vector) {
+        auto iter = std::find_if(vector.begin(), vector.end(), [](std::string &s) {
+            return s[0] >= 'A';
+        });
+        std::reverse(vector.begin(), iter);
+        std::reverse(iter, vector.end());
+        std::reverse(vector.begin(), vector.end());
+    };
+    sortVector(left.size() < right.size() ? right : left);
+}
+
+auto main() -> int {
+    auto vec1 = std::vector<std::string>{"ak1", "e2", "3a", "1b"};
+    auto vec2 = std::vector<std::string>{"3a", "1b", "2h", "ak1", "e2",};
+
+    fmt::println("{}", vec1);
+    fmt::println("{}", vec2);
+    exampleMain(vec1, vec2);
+    fmt::println("{}", vec2);
+}
