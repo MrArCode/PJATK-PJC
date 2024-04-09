1118fe515491a77be66f8b14727920d9dd9775c9
Artur Cegiełka-Posłuszny <a.c.posluszny@gmail.com>
2024-04-09 20:07:47 +0200

    egz

--- Egzamin/Ćwiczenia.cpp
+++ Egzamin/Ćwiczenia.cpp
@@ -3,15 +3,20 @@
 #include <fmt/ranges.h>
 #include <algorithm>
 #include <ranges>
+#include <set>
 
-auto main()->int{
-    auto vec = std::vector<int>{3, 5, 1, 4, 2, 0};
-
-
-    std::ranges::sort(vec.begin(),vec.begin()+vec.size()/2);
-    std::ranges::reverse(vec.begin()+vec.size()/2,vec.end());
-
-    fmt::println("{}",vec);
-
-
+namespace PJC {
+    auto modify(std::vector<int> &vec) -> void {
+        auto set = std::set<int>(vec.begin(), vec.end());
+        vec = std::vector<int>(set.begin(), set.end());
+    }
 }
+
+
+auto main() -> int {
+    auto vec = std::vector<int>{1,2,5,7,9,12};
+    auto iter = vec.begin();
+    vec.erase(iter);
+    fmt::print("{}", vec);
+}
+
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/czuro/CLionProjects/PJATK-PJC
+# Install script for directory: /Users/arturcegielka-posluszny/CLionProjects/PJATK-PJC
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Wyklad")
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,12 +34,12 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2023.3.3/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "/Library/Developer/CommandLineTools/usr/bin/objdump")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/Users/czuro/CLionProjects/PJATK-PJC/cmake-build-debug/_deps/fmt-build/cmake_install.cmake")
+  include("/Users/arturcegielka-posluszny/CLionProjects/PJATK-PJC/cmake-build-debug/_deps/fmt-build/cmake_install.cmake")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/czuro/CLionProjects/PJATK-PJC/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/arturcegielka-posluszny/CLionProjects/PJATK-PJC/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
